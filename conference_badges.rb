73984800a1d6002a811a08ef70d9b2528566df8d
Caleb R <cjr4912@gmail.com>
2020-07-22 18:02:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,6 @@ def batch_badge_creator(names)
   end
   
   return name_badges
-  
-  end
-end 
 
 def assign_rooms(names)
   names.map.with_index {|name, index| "Hello," + name "!" "You'll be assigned to room" + (index + 1) "!"}
