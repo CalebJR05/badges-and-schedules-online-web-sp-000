796fac669299e77f92c9e5e1ace5c5903e530fdc
Caleb R <cjr4912@gmail.com>
2020-07-22 17:37:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,5 @@
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-def batch_badge_creato(names)
+def batch_badge_creator(names)
+  name_badges = names.each |name|
+  puts "Hello, my name is" + name
   
